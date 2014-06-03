aee30c8c8d8f6db7dd0783b9cd16126537ed4086
aki33524 <aki33524@192.168.11.3>
2014-06-03 23:05:19 +0900

    Union-Find

--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,10 @@ def bitmasks(n):
 
 def generate_primes(n):
     "generate prime list less than n"
+    n += 1
     is_prime = [True]*n
+    is_prime[0] = False
+    is_prime[1] = False
     primes = []
     for i in range(2, n):
         if is_prime[i]:
@@ -31,12 +34,30 @@ def is_prime(n):
 
 def memoize(func):
     """decorator that chaches"""
-    import functools
-    @functools.wraps(func)
+    memoized_values = {}
     def wrapper(*args):
-        func._memoized_values = getattr(func, '_memoized_values', {})
-        key = (func,args)
-        if key not in func._memoized_values:
-            func._memoized_values[key] = func(*args)
-        return func._memoized_values[key]
+        nonlocal memoized_values
+        key = args
+        if key not in memoized_values:
+            memoized_values[key] = func(*args)
+        return memoized_values[key]
     return wrapper
+
+class UnionFind():
+    def __init__(self, n):
+        self._i2g = [i for i in range(n)]
+        self._g2i = [[i] for i in range(n)]
+    
+    def merge(self, ia, ib):
+        ga = self._i2g[ia]
+        gb = self._i2g[ib]
+        if len(self._g2i[ga]) < len(self._g2i[gb]):
+            ga, gb = gb, ga
+        for v in self._g2i[gb]:
+            self._i2g[v] = ga
+        self._g2i[ga] += self._g2i[gb]
+        self._g2i[gb] = []
+        
+    def is_same_group(self, ia, ib):
+        return self._i2g[ia] == self._i2g[ib]
+    
