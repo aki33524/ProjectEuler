5b6ca2683f556e31b7c5c2b551b56d4f1c4286c3
aki33524 <aki33524@192.168.11.3>
2014-06-03 23:04:28 +0900

    Union-Find

--- src/ProjectEuler.py
+++ src/ProjectEuler.py
@@ -1,9 +1,11 @@
 from utils import *
-import hashlib
 
 @memoize
 def fib(n):
     return n if n==0 or n==1 else fib(n-1)+fib(n-2)
 
 if __name__ == "__main__":
-    print(fib(100))
+    uf = UnionFind(10)
+    uf.merge(1, 2)
+    print(uf.is_same_group(1, 2))
+    print(uf.is_same_group(2, 4))
